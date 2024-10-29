79b438557bf08c415f4594652aa26376f2299c7f
jrollio <jrollio@outlook.com>
2024-10-28 21:05:47 -0500

    initial branch commit

--- az-104/astro.config.mjs
+++ az-104/astro.config.mjs
@@ -2,41 +2,51 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 
+import node from '@astrojs/node';
+
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        starlight({
-            components: {
-                TableOfContents: './src/components/TableOfContents.astro'
-                , MobileTableOfContents: './src/components/MobileTableOfContents.astro'
-            }
-            , customCss: [
-                './styles/custom.css'
-            ]
-            , favicon: '/images/favicon.svg'
-            , head: [
-                {
-                    tag: 'link'
-                    , attrs: {
-                        rel: 'icon'
-                        , href: '/images/favicon.ico'
-                        , sizes: '32x32'
-                    }
-                }
-            ]
-            , logo: {
-                src: './src/assets/MSCertifiedAssociate.png'
-                , replacesTitle: true
-            }
-            , tableOfContents: {
-                minHeadingLevel: 1
-                , maxHeadingLevel: 5
-            }
-            , title: 'AZ-104'
-            , titleDelimiter: ' — '
-            , social: {
-                github: 'https://github.com/withastro/starlight'
-            }
-        })
-    ]
+  integrations: [
+      starlight({
+          components: {
+              TableOfContents: './src/components/TableOfContents.astro'
+              , MobileTableOfContents: './src/components/MobileTableOfContents.astro'
+          }
+          , customCss: [
+              './styles/custom.css'
+          ]
+          , favicon: '/images/favicon.svg'
+          , head: [
+              {
+                  tag: 'link'
+                  , attrs: {
+                      rel: 'icon'
+                      , href: '/images/favicon.ico'
+                      , sizes: '32x32'
+                  }
+              }
+          ]
+          , logo: {
+              src: './src/assets/MSCertifiedAssociate.png'
+              , replacesTitle: true
+          }
+          , tableOfContents: {
+              minHeadingLevel: 1
+              , maxHeadingLevel: 5
+          }
+          , prerender: false
+          , title: 'AZ-104'
+          , titleDelimiter: ' — '
+          , social: {
+              github: 'https://github.com/withastro/starlight'
+          }
+      })
+  ],
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
