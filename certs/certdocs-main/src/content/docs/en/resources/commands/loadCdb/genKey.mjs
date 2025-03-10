552a46ce3d9d3e75bccc49055face243646c3245
jrollio <jrollio@outlook.com>
2025-03-10 16:13:40 +0000

    testing keygen

---
+++ certs/certdocs-main/src/content/docs/en/resources/commands/loadCdb/genKey.mjs
@@ -0,0 +1,42 @@
+'use strict';
+
+/*
+export EK="{ key_ops: [ 'encrypt', 'decrypt' ], ext: true, kty: 'oct', k: 'uwt310hdcp3HRPq7nVZKSoR29YujJ0A8OFdV_UZEdto', alg: 'A256CBC' }"
+Buffer.from(process.env.EK).toString('base64');
+
+const sc = require('node:crypto');
+key2 = await sc.subtle.generateKey( { name: 'AES-CBC', length: 256 }, true, ['encrypt','decrypt']);
+
+expkey = await sc.subtle.exportKey('jwk', key2 );
+
+impkey = await sc.subtle.importKey('jwk', true, ['encrypt','decrypt'], 'uwt310hdcp3HRPq7nVZKSoR29YujJ0A8OFdV_UZEdto');
+buf = Buffer.from(JSON.stringify(expkey), 'base64');
+buf.toString();
+Buffer.from(buf,'base64').toString('base64');
+*/
+
+import { readdir, readFile } from 'node:fs/promises';
+import 'dotenv/config';
+import { subtle } from 'node:crypto';
+
+
+// async function generateAesKey(length = 256) {
+//     return key;
+// } 
+
+const alg = 'AES-CBC';
+const klength = 256;
+// const iv = crypto.getRandomValues(new Uint8Array(16));
+// const key = await generateAesKey();
+const key = await subtle.generateKey({
+    name: alg,
+    length,
+    },
+    true,
+    ['encrypt', 'decrypt']
+);
+const expkey = await subtle.exportKey(format: 'jwk', key: key);
+// console.log(`Key: ${key}\nIv: ${iv}`);
+console.log(`Exported Key:\n${expkey}`);
+const impkey = await subtle.importKey(format: 'jwk', true, [ 'encrypt', 'decrypt'], process.argv[2]);
+console.log(`Imported Key:\n${impkey}`);
